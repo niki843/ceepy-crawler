edda7f6f1c0391071c45fa4cd84d49eede68ae7a
niki843 <niki.tornev96@gmail.com>
2025-03-29 19:38:53 +0200

    Set up alembic DB path through env variable

--- BE/app/alembic/env.py
+++ BE/app/alembic/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -20,10 +21,8 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 target_metadata = None
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+def get_url():
+    return os.getenv("DATABASE_URL")
 
 
 def run_migrations_offline() -> None:
@@ -57,6 +56,8 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    configuration = config.get_section(config.config_ini_section)
+    configuration["sqlalchemy.url"] = get_url()
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
