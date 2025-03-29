23cd3e6be73db0925837cd31818d6ab31d7091cb
niki843 <niki.tornev96@gmail.com>
2025-03-29 18:40:19 +0200

    Modified file structure for FE integration in future

--- BE/app/database.py
+++ BE/app/database.py
@@ -1,18 +1,17 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from app.config import DATABASE_URL
+from BE.app.config import DATABASE_URL
 
 # Create Async Engine
 engine = create_async_engine(DATABASE_URL, future=True, echo=True)
 
 # Session Local
-SessionLocal = sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
+async_session  = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 # Base Class
 Base = declarative_base()
 
 # Dependency function to get DB session
 async def get_db():
-    async with SessionLocal() as session:
+    async with async_session() as session:
         yield session
