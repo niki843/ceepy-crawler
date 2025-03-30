dff43c46e66f3b693485fd59d4bf8300db1a1177
niki843 <niki.tornev96@gmail.com>
2025-03-30 23:24:00 +0300

    Updated DTO to use HttpUrl for url validation

--- BE/app/schemas/screenshot.py
+++ BE/app/schemas/screenshot.py
@@ -1,6 +1,7 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
+
 
 # Request Model: Used for creating a screenshot entry
 class ScreenshotDTO(BaseModel):
-    start_url: str
+    start_url: HttpUrl
     extracted_links: int
