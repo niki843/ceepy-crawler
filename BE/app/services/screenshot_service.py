a28e7d27cde956afb611342499d11c91d25a0808
niki843 <niki.tornev96@gmail.com>
2025-03-30 23:06:06 +0300

    Added optimization to not re-fetch records if they are already fetched today

--- BE/app/services/screenshot_service.py
+++ BE/app/services/screenshot_service.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from venv import logger
 
 from playwright.async_api import async_playwright
-from sqlalchemy import select, update
+from sqlalchemy import select, update, desc
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.responses import FileResponse
 
@@ -18,21 +18,55 @@ class ScreenshotService:
     SCREENSHOT_DIR = "./app/utils/screenshots/"
 
     @classmethod
-    async def start_screenshots(cls, start_url: str , extracted_links: int, db_session: AsyncSession):
+    async def start_screenshots(
+        cls, start_url: str, extracted_links: int, db_session: AsyncSession
+    ):
+        screenshots = await db_session.execute(
+            select(Screenshot)
+            .filter(Screenshot.url == start_url)
+            .order_by(desc(Screenshot.created_at))
+            .limit(1)
+        )
+        screenshot = screenshots.scalars().first()
+
+        # Validate if the screenshots were created today, having in mind that they might be changed if they are older
+        # Check if there are enough screenshots depending on the required + 1 because of the start link
+        if (
+            screenshot.created_at.date() == datetime.now().date()
+            and len(fetch_file_names_in_path(screenshot.path)) <= extracted_links + 1
+        ):
+            return screenshot.id
+
         created_at = datetime.now()
         path = cls._generate_path(start_url, str(created_at))
-        screenshot = Screenshot(url=start_url, path=path, status=ScreenshotStatus.PENDING.value, created_at=created_at)
+        screenshot = Screenshot(
+            url=start_url,
+            path=path,
+            status=ScreenshotStatus.PENDING.value,
+            created_at=created_at,
+        )
         db_session.add(screenshot)
         await db_session.commit()
         await db_session.refresh(screenshot)
 
         # Create task to fetch screenshots
-        asyncio.create_task(cls.take_screenshot(start_url, extracted_links, path, db_session, str(screenshot.id)))
+        asyncio.create_task(
+            cls.take_screenshot(
+                start_url, extracted_links, path, db_session, str(screenshot.id)
+            )
+        )
 
         return screenshot.id
 
     @classmethod
-    async def take_screenshot(cls, start_url: str , extracted_links: int, path: str, db_session: AsyncSession, id:str):
+    async def take_screenshot(
+        cls,
+        start_url: str,
+        extracted_links: int,
+        path: str,
+        db_session: AsyncSession,
+        id: str,
+    ):
         async with async_playwright() as playwright:
             browser = await playwright.chromium.launch()
             page = await browser.new_page()
@@ -42,7 +76,9 @@ class ScreenshotService:
             await page.screenshot(path=path + file_name, type="png")
 
             # Fetch all links
-            links = await page.locator("a").evaluate_all("elements => elements.map(el => el.href)")
+            links = await page.locator("a").evaluate_all(
+                "elements => elements.map(el => el.href)"
+            )
 
             # Remove duplicates and invalid urls
             links = list(set(links))
@@ -67,17 +103,28 @@ class ScreenshotService:
 
             await browser.close()
 
-        await db_session.execute(update(Screenshot).where(Screenshot.id==id).values(status=ScreenshotStatus.DONE.value))
+        await db_session.execute(
+            update(Screenshot)
+            .where(Screenshot.id == id)
+            .values(status=ScreenshotStatus.DONE.value)
+        )
         await db_session.commit()
 
     @classmethod
     def _generate_path(cls, url: str, created_at: str):
-        path = cls.SCREENSHOT_DIR + get_host_from_url(url) + sanitize_str(created_at, "_") + "/"
+        path = (
+            cls.SCREENSHOT_DIR
+            + get_host_from_url(url)
+            + sanitize_str(created_at, "_")
+            + "/"
+        )
         return path
 
     @classmethod
     async def get_screenshot(cls, screenshot_id: str, db_session: AsyncSession):
-        screenshots = await db_session.execute(select(Screenshot).filter(Screenshot.id == screenshot_id))
+        screenshots = await db_session.execute(
+            select(Screenshot).filter(Screenshot.id == screenshot_id)
+        )
         screenshot = screenshots.scalars().first()
 
         if screenshot.status != ScreenshotStatus.DONE.value:
@@ -86,6 +133,8 @@ class ScreenshotService:
         files = []
         for file in fetch_file_names_in_path(screenshot.path):
             filename = os.fsdecode(file)
-            files.append(FileResponse(screenshot.path + filename, media_type="image/png"))
+            files.append(
+                FileResponse(screenshot.path + filename, media_type="image/png")
+            )
 
         return files
