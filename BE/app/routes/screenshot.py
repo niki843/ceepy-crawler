d076c4361df2b93f7d95186b69319e6959eb4c15
niki843 <niki.tornev96@gmail.com>
2025-03-30 23:25:17 +0300

    Formating

--- BE/app/routes/screenshot.py
+++ BE/app/routes/screenshot.py
@@ -6,11 +6,15 @@ from app.dependencies.core import DBSessionDep
 
 router = APIRouter()
 
+
 @router.post("/screenshot")
 async def capture_screenshot(screenshot_dto: ScreenshotDTO, db_session: DBSessionDep):
     """Captures a screenshot of the given URL."""
-    screenshot_id = await ScreenshotService.start_screenshots(screenshot_dto.start_url, screenshot_dto.extracted_links, db_session)
+    screenshot_id = await ScreenshotService.start_screenshots(
+        str(screenshot_dto.start_url), screenshot_dto.extracted_links, db_session
+    )
     return {"screenshot_id": screenshot_id}
+
 
 @router.get("/screenshot/{id}")
 async def fetch_screenshot(id: str, db_session: DBSessionDep):
